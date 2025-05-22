6c9ac366ac7bc92eebf53bdc2e5adbf33bbc183e
jaddesuarez <jaddesuarez@gmail.com>
2025-05-22 17:13:18 +0200

    tanstack/react-query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
