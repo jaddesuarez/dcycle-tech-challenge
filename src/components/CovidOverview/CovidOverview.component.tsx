cfd0695f6fcdf632cd7d593a80141788a6af455e
jaddesuarez <jaddesuarez@gmail.com>
2025-05-23 13:39:55 +0200

    Update CovidOverview.component.tsx

--- src/components/CovidOverview/CovidOverview.component.tsx
+++ src/components/CovidOverview/CovidOverview.component.tsx
@@ -7,7 +7,7 @@ export const CovidOverview = ({
   covidOverview: ICovidOverview;
 }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       <CovidOverviewCard
         title="Total Cases"
         value={covidOverview.totalCases.toLocaleString()}
